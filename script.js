3efa5f749b9c1ccef4f2b5e40efc5de06e84154c
TomAk1993 <96138157+TomAk1993@users.noreply.github.com>
2022-06-18 21:24:30 +0100

    scroll back to the top button added back in

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ links.forEach((link) => {
             
     })
 });
+
+
+// Button to get users back to the top of the webpage 
+
+const toTop = document.querySelector('.toTop');
+
+    window.addEventListener('scroll', () => {
+        if ( window.pageYOffset > 500){
+            toTop.classList.add('active');
+        } else {
+            toTop.classList.remove('active');
+        }
+    });
+
+toTop.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});  
